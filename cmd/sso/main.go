6d724436bb5cb8dd517e60c3c8bc69c6d0b03672
Alexander Orlov <eadenink@gmail.com>
2024-09-05 14:51:27 +0400

    Define sso microservice's entry point

---
+++ cmd/sso/main.go
@@ -0,0 +1,11 @@
+package sso
+
+func main() {
+	// TODO: Init config
+
+	// TODO: Init logger
+
+	// TODO: Init app
+
+	// TODO: Run gRPC server
+}
--- protos/gen/go/sso/sso_grpc.pb.go
+++ protos/gen/go/sso/sso_grpc.pb.go
@@ -4,7 +4,7 @@
 // - protoc             v5.27.3
 // source: sso/sso.proto
 
-package sso
+package ssov1
 
 import (
 	context "context"
