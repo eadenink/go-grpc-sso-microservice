26a71b1c6cb7165ef62ce54079a42a7676fd7ecb
Alexander Orlov <eadenink@gmail.com>
2024-09-05 16:21:44 +0400

    Init logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/eadenink/go-grpc-sso-microservice/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	// TODO: Init logger
+	logger := setupLogger(cfg.Env)
 
 	// TODO: Init app
 
 	// TODO: Run gRPC server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return logger
 }
